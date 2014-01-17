0de09bd024bbefb04f552dc85f710a43d5cf413c
Graeme Mathieson <mathie@woss.name>
2014-01-17 18:57:53 +0000

    Make docker listen on a TCP port and forward that back to the Mac host.
    Now I can use the docker client directly on MacOS X with this VM
    running... :)

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,8 @@
 Vagrant.configure('2') do |config|
   config.vm.box = "ubuntu-13.10-server-amd64"
 
+  config.vm.network :forwarded_port, guest: 4243, host: 4243
+
   config.vm.provision :shell do |shell|
     shell.path = 'bin/bootstrap.sh'
   end
--- modules/docker/manifests/config.pp
+++ modules/docker/manifests/config.pp
@@ -3,4 +3,13 @@ class docker::config {
     'vagrant':
       groups => docker;
   }
+
+  file {
+    '/etc/default/docker':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => template('docker/etc-default-docker.sh.erb');
+  }
 }
